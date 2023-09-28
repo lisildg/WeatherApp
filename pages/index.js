4292c19e89ed40a121e9bc14a371e58ae14ea6e0
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-09-27 22:05:34 -0300

    final push

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import dotenv from 'dotenv';
 import Current from "./components/Current";
 import WeatherDetails from "./components/WeatherDetails";
-import WeekForecast from "./components/WeekForecast";
+import WeekForecast from "./components/Weekforecast";
 dotenv.config();
 export default function Home() {
 const [data, setData] = useState({});
